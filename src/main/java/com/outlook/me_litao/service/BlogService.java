131b055f87d1b52bf439ff5d4289b57b81432834
ximu-tao <me_litao@outlook.com>
2021-09-10 18:25:40 +0800

    Service 层 Blog 部分实现

---
+++ src/main/java/com/outlook/me_litao/service/BlogService.java
@@ -0,0 +1,23 @@
+package com.outlook.me_litao.service;
+
+import com.outlook.me_litao.bean.DataPage;
+import com.outlook.me_litao.pojo.Blog;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Map;
+
+/**
+ * @program: ssm-blog
+ * @description: description
+ * @author: Ximu-Tao
+ * @create: 2021-09-04 13:28
+ **/
+public interface BlogService {
+    void addBlog( @Param("blog") Blog b);
+
+    void delBlog( @Param("bid") int bid );
+
+    DataPage selectBlogs(Map m);
+
+    int selectBlogsCount(Map m);
+}
---
+++ src/main/java/com/outlook/me_litao/service/BlogServiceImpl.java
@@ -0,0 +1,92 @@
+package com.outlook.me_litao.service;
+
+
+import com.outlook.me_litao.bean.DataPage;
+import com.outlook.me_litao.dao.BlogMapper;
+import com.outlook.me_litao.pojo.Blog;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @program: ssm-blog
+ * @description: description
+ * @author: Ximu-Tao
+ * @create: 2021-09-06 12:18
+ **/
+public class BlogServiceImpl implements BlogService{
+    private SqlSessionFactory sqlSessionFactory;
+    private BlogMapper blogMapper;
+
+    public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
+        this.sqlSessionFactory = sqlSessionFactory;
+        this.blogMapper = sqlSessionFactory.openSession().getMapper(BlogMapper.class);
+    }
+
+    public void setBlogMapper(BlogMapper blogMapper) {
+        this.blogMapper = blogMapper;
+    }
+
+
+
+    @Override
+    public void addBlog(Blog b) {
+        blogMapper.addBlog(b);
+    }
+
+    @Override
+    public void delBlog(int bid) {
+        blogMapper.delBlog(bid);
+    }
+
+    @Override
+    public DataPage selectBlogs(Map m) {
+        if ( m == null){
+            m = new HashMap<String,Object>();
+        }
+
+        if ( !m.containsKey( "page") ){
+            m.put("page" , 1 );
+        }
+
+        m.put("offSet" , 10 * ((int)m.get("page")-1 )  );
+        m.put("limit" , 10 );
+
+        int count  = blogMapper.selectBlogsCount( m );
+        DataPage page = new DataPage<Blog>();
+
+
+
+        List<Blog> blogs2 = blogMapper.selectBlogs(m);
+        List<Map<String,String>> blogs = blogs2.stream().map( blog -> {
+            HashMap<String, String> map = new HashMap<>();
+            map.put("id" , String.valueOf(blog.getId()));
+            map.put( "avatar" , blog.getPublisher().getAvatar() );
+            map.put( "name" ,  blog.getPublisher().getName()  );
+            map.put( "publish_time" ,  blog.getPublishTime().toString()  );
+            map.put( "publisher_id" , String.valueOf(blog.getPublisher().getId()));
+            map.put( "title" ,  blog.getTitle()  );
+            map.put( "content" ,  blog.getContent()  );
+
+            return map;
+        }).collect(Collectors.toList());
+
+        page.setList( blogs );
+        page.setPagenum((Integer) m.get("page"));
+        page.setPagesnum( (count/10)+ ((count%10)==0?0:1) );
+        page.addParameter( "publisher_id" , (String) m.get("publisher_id"));
+
+        page.addParameter("publish_time" , (String) m.get("publish_time"));
+
+
+        return page;
+    }
+
+    @Override
+    public int selectBlogsCount(Map m) {
+        return blogMapper.selectBlogsCount(m);
+    }
+}
