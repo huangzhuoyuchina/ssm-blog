0e084bac0c520b7c48f61b7583bb151e1c182413
ximu-tao <me_litao@outlook.com>
2021-09-11 16:35:53 +0800

    项目前端部分

---
+++ web/js/apiurl.js
@@ -0,0 +1,12 @@
+apiUrlList = {
+    "checkserver" : "./api/check/checkserver.php",
+    "hasname" : "./api/check/hasname.php",
+    "unlog" : "./api/unlog.php",
+    "getuserdata" : "./api/get/getuserdata.php",
+    // "getbloglist" : "./api/get/getbloglist.php",
+    "getbloglist" : "http://localhost:8080/api/get/getbloglist",
+    "login" : "./api/login.php",
+    "uploadAvatar" : "./api/upload/avatar.php",
+    "signup" : "./api/signup.php",
+    "publishblog" : "./api/publish/publishblog.php"
+}
